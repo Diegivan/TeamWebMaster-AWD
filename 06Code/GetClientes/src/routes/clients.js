81902d59b8d467f3eade9f6c4ed0fba99439589a
Christian Novoa <79382364+Cpnovoa1@users.noreply.github.com>
2021-12-28 22:56:05 -0500

    Create Client REST: GET, POST, PUT, DELETE

--- 06Code/GetClientes/src/routes/clients.js
+++ 06Code/GetClientes/src/routes/clients.js
@@ -44,7 +44,22 @@ router.post('/clients/new', async (req, res) => {
 router.get('/admin/clients', async (req, res) => {
     const clients = await Client.find({}).lean();
     res.render('clients/allClients', { clients });
-    console.log(clients);
+});
+
+router.get('/admin/clients/edit/:id', async (req, res) => {
+    const client = await Client.findById(req.params.id).lean();
+    res.render('clients/editClient', { client });
+});
+
+router.put('/clients/editClient/:id', async (req, res) => {
+    const { firstName, lastName, ci, email, birthDate } = req.body;
+    await Client.findByIdAndUpdate(req.params.id, { firstName, lastName, ci, email, birthDate }).lean();
+    res.redirect('/admin/clients');
+});
+
+router.delete('/admin/clients/delete/:id', async (req, res) => {
+    await Client.findByIdAndDelete(req.params.id).lean();
+    res.redirect('/admin/clients');
 });
 
 module.exports = router;

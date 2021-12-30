c1dd30be9429ecc154cefdf0cc3a63794b95f6e9
diegivan <diegivan17@hotmail.com>
2021-12-30 15:35:30 -0500

    user changes

--- 06Code/FastSplash v2.0/src/routes/users.js
+++ 06Code/FastSplash v2.0/src/routes/users.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-
+const User = require('../models/user');
 router.get('/login',(req, res) => {
     res.render('./users/login');
 });
@@ -8,7 +8,12 @@ router.get('/login',(req, res) => {
 router.get('/register',(req, res) => {
     res.render('./users/register');
 });
-
+//users-admin
+router.get('/admin/users',async(req, res) => {
+    const user = await User.find({}).lean();
+    res.render('users/all-users',{ user });
+    
+});
 // To recibe data
 router.post('/register',(req, res) => {
     console.log(req.body);

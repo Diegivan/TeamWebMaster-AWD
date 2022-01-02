ee1768c23dd6ccd1055b8fddc259f38e5d523bf4
Christian Paul Novoa Chico <cnovoa10d@gmail.com>
2022-01-01 22:25:13 -0500

    New Error display and Sessions

--- 06Code/FastSplash v2.0/src/index.js
+++ 06Code/FastSplash v2.0/src/index.js
@@ -5,11 +5,13 @@ const exphbs = require('express-handlebars');
 const methodOverride = require('method-override');
 const session = require('express-session');
 const flash = require('connect-flash');
+const passport = require('passport')
 
 // Initializations
 const app = express();
 const port = process.env.PORT || 8085;
 require('./database');
+require('./config/passport');
 
 // Settings
 app.set('port', port);
@@ -33,11 +35,15 @@ app.use(session({
     saveUninitialized: true
 }));
 app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Global Variables
 app.use((req, res, next) => {
     res.locals.success_msg = req.flash('success_msg');
     res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    res.locals.user = req.user || null;
     next();
 });
 

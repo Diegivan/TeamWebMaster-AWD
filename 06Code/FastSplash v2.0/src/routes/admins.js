8e05bb6746fb00770cb4d974a8026b4f7dd413f3
Christian Novoa <79382364+Cpnovoa1@users.noreply.github.com>
2022-01-05 12:55:35 -0500

    Admin Validation

--- 06Code/FastSplash v2.0/src/routes/admins.js
+++ 06Code/FastSplash v2.0/src/routes/admins.js
@@ -11,23 +11,90 @@ router.get('/admin/add', isAuthenticated , (req, res) => {
 router.post('/admin/new-admin', isAuthenticated , async(req, res) => {
     const { name, lastname, CI, userName, password, confirmPassword, rol}=req.body;
     const errors = [];
-    if(!name){
-        errors.push({text:'Ingrese nombre'});
+
+    if (!name) {
+        errors.push({ text: 'Debe ingresar su nombre' });
+    } else {
+        if (!/^[a-zA-ZÀ-ÿ\s]{1,50}$/.test(name)) {
+            errors.push({ text: 'El nombre solo puede contener letras' });
+        }
     }
-    if(!lastname){
-        errors.push({text:'Ingrese apellido'});
+    if (!lastname) {
+        errors.push({ text: 'Debe ingresar su apellido' });
+    } else {
+        if (!/^[a-zA-ZÀ-ÿ\s]{1,50}$/.test(lastname)) {
+            errors.push({ text: 'El apellido solo puede contener letras' });
+        }
     }
-    if(!CI){
-        errors.push({text:'Ingrese cedula'});
+    if (!CI) {
+        errors.push({ text: 'Debe ingresar su cédula' });
+    } else {
+        var total = 0;
+        var longitud = CI.length;
+        var longCheck = longitud - 1;
+        var message = "";
+        var flag = false;
+
+        if (CI !== "" && longitud == 10) {
+            if (isNaN(CI)) {
+                errors.push({ text: "La CI solo puede contener números" });
+            }
+            else {
+                for (var i = 0; i < longCheck; i++) {
+                    if (i == 0) {
+                        let firstNumbers = parseInt(CI.charAt(i)) * 10 + parseInt(CI.charAt(i + 1));
+                        if (firstNumbers >= 25) {
+                            errors.push({ text: "La CI no corresponde a ninguna provincia" });
+                        }
+                    }
+                    if (i % 2 === 0) {
+                        var aux = CI.charAt(i) * 2;
+                        if (aux > 9) aux -= 9;
+                        total += aux;
+                    } else {
+                        total += parseInt(CI.charAt(i));
+                    }
+                }
+
+                total = total % 10 ? 10 - total % 10 : 0;
+
+                if (CI.charAt(longitud - 1) == total) {
+                } else {
+                    errors.push({ text: "Debe ingresar una CI ecuatoriana" });
+                }
+            }
+        }
+        else {
+            errors.push({ text: "La CI debe tener 10 dígitos" });
+        }
     }
-    if(!userName || userName.length<5){
-        errors.push({text:'Ingrese un nombre de usuario, debe contener mas de 5 caracteres '});
+    if (!userName) {
+        errors.push({ text: 'Debe ingresar su nombre de usuario' });
+    } else {
+        if (!/^[a-zA-ZÀ-ÿ0-9-_]{1,20}$/.test(userName)) {
+            errors.push({ text: 'El nombre de usuario solo puede contener caracteres alfanuméricos y _' });
+        }
     }
-    if(!password || password.length<5){
-        errors.push({text:'Ingrese una contrasena, debe ser mayor a 5 caracteres'});
-    }
-    if (password != confirmPassword) {
-        errors.push({ text: 'Error: Las contraseñas no coinciden' });
+    if (!password) {
+        errors.push({ text: 'Debe ingresar una contraseña' });
+    } else {
+        if (!/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])\S{8,16}$/.test(password)) {
+            errors.push({ text: 'La contraseña debe tener mínimo una mayúscula, una minúscula y un número' });
+            if (password.length < 8) {
+                errors.push({ text: 'La contraseña debe ser mayor a 8 caracteres' });
+            }
+            if (password.length > 16) {
+                errors.push({ text: 'La contraseña debe ser menor a 16 caracteres' });
+            }
+        } else {
+            if (!confirmPassword) {
+                errors.push({ text: 'Debe confirmar la contraseña' });
+            } else {
+                if (password != confirmPassword) {
+                    errors.push({ text: 'Las contraseñas no coinciden' });
+                }
+            }
+        }
     }
     if(errors.length>0)
     {

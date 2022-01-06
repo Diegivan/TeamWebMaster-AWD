3b50e8d310674e5a1f85e5b804724c07b866e93b
fpaezespe37 <freddyjair37@gmail.com>
2022-01-06 16:54:09 -0500

    Edit appointments and PDF generator

--- 06Code/FastSplash v2.0/src/routes/appointment.js
+++ 06Code/FastSplash v2.0/src/routes/appointment.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const { render } = require('express/lib/response');
-const Services = require('../models/Services');
+const Service = require('../models/Services');
 const Appointment = require('../models/Appointments');
 const router = express.Router();
+const PDF = require('pdfkit-construct');
+const fs = require ('fs');
 const passport = require('passport')
 const { isAuthenticated } = require('../helpers/auth');
+const { ClientRequest } = require('http');
 
 
 
@@ -76,5 +79,113 @@ router.post('/appointment',async(req, res) => {
     const appointment = await Appointment.find({}).lean();
     res.render('appointments/all-appointment',{ appointment });
 });
+
+router.get('/admin/appointments',async(req, res) => {
+    const appointment = await Appointment.find({}).lean();
+    res.render('appointments/all-appointment',{ appointment });
+});
+
+router.get('/admin/edit-appointments/:id',async(req, res) => {
+    const appointment = await Appointment.findById(req.params.id).lean();
+    res.render('appointments/edit-appointment',{ appointment });
+    
+});
+router.put('/appointments/edit-appointment/:id',async(req, res) => {
+    const {Name, Plate, status}=req.body;
+    await Appointment.findByIdAndUpdate(req.params.id,{Name, Plate, status}).lean();
+    req.flash('success_msg', 'Cita modificado satisfactoriamente');
+    res.redirect('/admin/appointments')
+});
+
+router.get('/get-factura/:id', async(req, res)=> {
+    
+    const appointment = await Appointment.findById(req.params.id).lean();
+    const service = await Service.findById(req.params.id).lean();
+    const doc = new PDF({bufferPage: true});
+    const filename = `Factura${Date.now()}.pdf`;
+    const stream = res.writeHead(200, {
+        'Content-Type': 'application/pdf',
+        'Content-disposition': `attachment;filename=${filename}`
+    });
+
+    doc.on('data', (data) => {stream.write(data)});
+    doc.on('end', ()=>{stream.end()});
+
+    const citas = [
+       {
+           Direccion: `${appointment.Adress}`,
+           Referencia: `${appointment.Reference}`,
+           Placa: `${appointment.Plate}`,
+           Servicio: `${appointment.services}`,
+           Hora: `${appointment.hours}`,
+           Observacion: `${appointment.Obs}`,
+        } 
+    ];
+
+    doc.setDocumentHeader({
+        height: '16'
+    }, ()=> {
+        
+        doc.fontSize(20).text('FACTURA DE SERVICIO FATSPLASH', {
+            width: 420,
+            align: 'center'
+        });
+    });
+    doc.fontSize(20).text('FACTURA DE SERVICIO FATSPLASH', {
+        width: 420,
+        align: 'center'
+    });
+    doc.fontSize(12);
+
+    doc.text(`Cliente: ${appointment.Name} `, {
+        width: 420,
+        align: 'left'
+    });
+    doc.text(`Direccion: ${appointment.Adress}`, {
+        width: 420,
+        align: 'left'
+    });
+    doc.text(`Placa: ${appointment.Plate} `, {
+        width: 420,
+        align: 'left'
+    });
+    doc.text(`Servicio: ${appointment.services} `, {
+        width: 420,
+        align: 'left'
+    });
+    doc.text(`Hora: ${appointment.hours} `, {
+        width: 420,
+        align: 'left'
+    });
+    doc.text(`Observacion: ${appointment.Obs} `, {
+        width: 420,
+        align: 'left'
+    });
+
+    doc.addTable([
+        {key: 'Direccion', label: 'Direccion', align:'left'},
+        {key: 'Referencia', label: 'Refrencia', align:'left'},
+        {key: 'Placa', label: 'Placa', align:'left'},
+        {key: 'Servicio', label: 'Servicio', align:'left'},
+        {key: 'Hora', label: 'Hora', align:'left'},
+        {key: 'Observacion', label: 'Observacion', align:'left'},
+
+    ], citas, {
+        
+            border: null,
+            width: "fill_body",
+            striped: true,
+            stripedColors: ["#f6f6f6", "#d6c4dd"],
+            cellsPadding: 10,
+            marginLeft: 45,
+            marginRight: 45,
+            headAlign: 'left'
+    });
+
+    doc.render();
+
+    doc.end();
+
+})
 
 module.exports = router;

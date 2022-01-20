1bee5c76b276f0d62472580450f1123920cb0af4
Christian Novoa <79382364+Cpnovoa1@users.noreply.github.com>
2022-01-19 19:33:59 -0500

    Update uris.js

--- 06Code/FastSplash/fastsplash_api/src/routes/uris.js
+++ 06Code/FastSplash/fastsplash_api/src/routes/uris.js
@@ -13,42 +13,42 @@ const ServiceController = require('../controllers/services');
 const UserController = require('../controllers/user');
 
 const AppointmentController = require('../controllers/appointments');
-const { isAuthenticated, isAuthenticatedUser } = require('../helpers/auth');
+const {  isAuthenticatedUser } = require('../helpers/auth');
 const { loginAuthenticate } = require("../controllers/user");
 
 //Client Uris
-router.get('/admin/clients', isAuthenticated, ClientController.allClients)
-router.get('/admin/clients/edit/:id', isAuthenticated, ClientController.getClient);
-router.post('/clients/new',  isAuthenticated, ClientController.addClient);
-router.put('/clients/editClient/:id',  isAuthenticated, ClientController.editClient);
-router.delete('/admin/clients/delete/:id', isAuthenticated, ClientController.deleteClient);
+router.get('/admin/clients', ClientController.allClients)
+router.get('/admin/clients/edit/:id', ClientController.getClient);
+router.post('/clients/new',  ClientController.addClient);
+router.put('/clients/editClient/:id',  ClientController.editClient);
+router.delete('/admin/clients/delete/:id', ClientController.deleteClient);
 
 //Admin Uris
-router.get('/admin/admins', isAuthenticated, AdminController.allAdmins);
-router.post('/admin/new-admin', isAuthenticated, AdminController.addAdmins);
-router.get('/admin/edit-admins/:id', isAuthenticated, AdminController.editAdminsRender);
-router.put('/admin/edit-admins/:id', isAuthenticated, AdminController.editAdmin);
-router.delete('/admin/delete/:id', isAuthenticated, AdminController.deleteAdmin);
+router.get('/admin/admins',  AdminController.allAdmins);
+router.post('/admin/new-admin',  AdminController.addAdmins);
+router.get('/admin/edit-admins/:id',  AdminController.editAdminsRender);
+router.put('/admin/edit-admins/:id', AdminController.editAdmin);
+router.delete('/admin/delete/:id', AdminController.deleteAdmin);
 
 //Services Uris
-router.get('/admin/services',isAuthenticated, ServiceController.allServices);
-router.post('/admin/new-services',isAuthenticated, ServiceController.newServices);
-router.get('/admin/edit-services/:id', isAuthenticated, ServiceController.editServiceRender);
-router.put('/services/edit-service/:id', isAuthenticated, ServiceController.editService);
-router.delete('/admin/delete-service/:id', isAuthenticated, ServiceController.deleteService);
+router.get('/admin/services', ServiceController.allServices);
+router.post('/admin/new-services', ServiceController.newServices);
+router.get('/admin/edit-services/:id',  ServiceController.editServiceRender);
+router.put('/services/edit-service/:id',  ServiceController.editService);
+router.delete('/admin/delete-service/:id',  ServiceController.deleteService);
 
 //User Uris
-router.get('/admin/users',isAuthenticated, UserController.allUsers);
+router.get('/admin/users', UserController.allUsers);
 
 // Appointment Uris
 router.get('/appointment', AppointmentController.allAppointments);
-router.get('/historial/appointments', isAuthenticatedUser, AppointmentController.historyAppointments);
-router.get('/admin/appointments', isAuthenticated, AppointmentController.allAppointmentsUsers);
-router.get('/admin/edit-appointments/:id', isAuthenticated, AppointmentController.getAppointment);
+router.get('/historial/appointments', AppointmentController.historyAppointments);
+router.get('/admin/appointments',  AppointmentController.allAppointmentsUsers);
+router.get('/admin/edit-appointments/:id',  AppointmentController.getAppointment);
 router.get('/get-factura/:id', AppointmentController.getBill);
-router.post('/appointment', isAuthenticated, AppointmentController.addAppointment); 
+router.post('/appointment',  AppointmentController.addAppointment); 
 router.put('/appointments/edit-appointment/:id', AppointmentController.editAppointment);
-router.delete('/appointments/delete/:id', isAuthenticated, AppointmentController.deleteAppointment);
+router.delete('/appointments/delete/:id',  AppointmentController.deleteAppointment);
 
 // Login
 router.post('/login', passport.authenticate('local'),

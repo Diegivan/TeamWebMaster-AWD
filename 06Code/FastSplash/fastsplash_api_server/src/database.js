9481600f90f032cb9c2248f361e263d03c6761ce
Christian Novoa <79382364+Cpnovoa1@users.noreply.github.com>
2022-02-08 20:17:53 -0500

    correction

--- 06Code/FastSplash/fastsplash_api_server/src/database.js
+++ 06Code/FastSplash/fastsplash_api_server/src/database.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 require("dotenv").config();
 
 mongoose
-    .connect(process.env.MONGODB_URI)
+    .connect("mongodb+srv://admin:admin@clusteraws.6k7qv.mongodb.net/FastSplash?retryWrites=true&w=majority")
     .then(() => console.log('Connected to MongoDB Atlas'))
     .catch((error) => console.error(error));
